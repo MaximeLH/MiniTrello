237fa5d427ef03bdc99fa9c220841c6505cf6465
Stagiaire11 <Stagiaire11@STAGIAIRE11>
2016-11-24 11:58:25 +0100

    tableau sup

--- MiniTrello/MiniTrello/View/FrmTableau.Designer.cs
+++ MiniTrello/MiniTrello/View/FrmTableau.Designer.cs
@@ -76,15 +76,6 @@
             this.pnlAjout.Size = new System.Drawing.Size(200, 100);
             this.pnlAjout.TabIndex = 7;
             // 
-            // txtTitreTableau
-            // 
-            this.txtTitreTableau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTitreTableau.Location = new System.Drawing.Point(12, 12);
-            this.txtTitreTableau.Name = "txtTitreTableau";
-            this.txtTitreTableau.Size = new System.Drawing.Size(109, 26);
-            this.txtTitreTableau.TabIndex = 8;
-            this.txtTitreTableau.Text = "Titre";
-            // 
             // txtAjout
             // 
             this.txtAjout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
