ab56e16e2aff247c8f0f26b636e41d2dab743f74
ambr <ambrault@gmail.com>
2016-11-25 10:01:46 +0100

    ctlajoutcarte

---
+++ MiniTrello/MiniTrello/View/CtlAjoutCarte.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiniTrello.Model;
+
+namespace MiniTrello.View
+{
+    public partial class CtlAjoutCarte : UserControl
+    {
+        public CtlAjoutCarte()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAjoutCarte_Click(object sender, EventArgs e)
+        {
+            Carte c = new Carte();
+            c.Titre = txtBoxTitreCarte.Text;
+            CtlCarte ctCarte = new CtlCarte();
+            
+        }
+    }
+}
